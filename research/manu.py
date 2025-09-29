1c3049e79272713dfb9f6157696f522b0beda210
manumanoj29 <lnmanoj25@gmail.com>
2025-09-29 07:42:39 +0530

    Update manu.py

--- research/manu.py
+++ research/manu.py
@@ -1,13 +1,1 @@
-# x=[i**2 for i in range(10) if i%2==0]
-# print(x)#List comprehension
-l={1,3,4,2,3,5,6,7,8,9}
-print(l)
-l.add(111)
-print(l)
-l.remove(3)
-print(l)
-#inter section of two sets
-n={45,54,6,7,4,56,3,8}
-print(l &n)
-#sub set of the given set
-print(l.issubset(n))
+
